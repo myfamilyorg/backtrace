0af26dd65ae250dc755b7c86c9badf331188d4ed
Christopher Gilliard <christopher.gilliard@gmail.com>
2025-05-06 18:18:01 -0700

    add backtrace impls

---
+++ rust/lib.rs
@@ -0,0 +1,57 @@
+#![no_std]
+
+extern crate ffi;
+
+use core::ptr::{null, null_mut};
+use ffi::{backtrace, gen_backtrace, getenv};
+pub const MAX_BACKTRACE_ENTRIES: usize = 128;
+
+#[repr(C)]
+#[derive(Clone)]
+pub struct Backtrace {
+    entries: [*mut (); MAX_BACKTRACE_ENTRIES],
+    size: i32,
+}
+
+impl Backtrace {
+    pub fn new() -> Self {
+        let mut ret = Backtrace {
+            entries: [null_mut(); MAX_BACKTRACE_ENTRIES],
+            size: 0,
+        };
+        ret.capture();
+        ret
+    }
+
+    pub const fn init() -> Self {
+        Self {
+            entries: [null_mut(); MAX_BACKTRACE_ENTRIES],
+            size: 0,
+        }
+    }
+
+    pub fn capture(&mut self) {
+        let size = unsafe {
+            if getenv("RUST_BACKTRACE\0".as_ptr()).is_null() {
+                0
+            } else {
+                backtrace(self.entries.as_mut_ptr(), MAX_BACKTRACE_ENTRIES as i32)
+            }
+        };
+        self.size = size;
+    }
+
+    pub unsafe fn as_ptr(&self) -> *const u8 {
+        if self.size <= 0 {
+            null()
+        } else {
+            unsafe { gen_backtrace(self.entries.as_ptr(), self.size) }
+        }
+    }
+}
+
+pub fn real_main(_argc: i32, _argv: *const *const i8) -> i32 {
+    let mut bt = Backtrace::new();
+    bt.capture();
+    0
+}
