d65aae064bf182cc213628c4c98369c906df6537
Christopher Gilliard <christopher.gilliard@gmail.com>
2025-05-07 23:59:54 -0700

    remove debug

--- rust/lib.rs
+++ rust/lib.rs
@@ -1,11 +1,7 @@
 #![no_std]
 
 extern crate ffi;
-use core::fmt::Result as FmtResult;
-use core::fmt::{Debug, Formatter};
 use core::ptr::{null, null_mut};
-use core::slice::from_raw_parts;
-use core::str::from_utf8_unchecked;
 use ffi::{backtrace, gen_backtrace, getenv};
 
 pub const MAX_BACKTRACE_ENTRIES: usize = 128;
@@ -15,32 +11,6 @@ pub const MAX_BACKTRACE_ENTRIES: usize = 128;
 pub struct Backtrace {
     entries: [*mut (); MAX_BACKTRACE_ENTRIES],
     size: i32,
-}
-
-impl Debug for Backtrace {
-    fn fmt(&self, f: &mut Formatter<'_>) -> FmtResult {
-        unsafe {
-            let bt = self.as_ptr();
-            let s = if bt.is_null() {
-                "Backtrace disabled. To enable export RUST_BACKTRACE=1."
-            } else {
-                let len = ffi::cstring_len(bt);
-                let slice = from_raw_parts(bt, len as usize);
-                from_utf8_unchecked(slice)
-            };
-            match write!(f, " {}", s) {
-                Ok(_) => {
-                    ffi::release(bt);
-                    Ok(())
-                }
-
-                Err(e) => {
-                    ffi::release(bt);
-                    Err(e)
-                }
-            }
-        }
-    }
 }
 
 impl Backtrace {
